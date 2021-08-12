bfa488a758a2bb9e3d540151fe622159de81f76e
Vic Khov <victorKhovTko@yandex.ru>
2021-08-12 09:05:35 +0300

    added small nim impl

---
+++ rt-in-one-weekend/nim/creating_an_image_file.nim
@@ -0,0 +1,34 @@
+import stb_image/write as stbiw
+import strformat
+import bitops # bitwise operations
+import freeimage
+
+let image_width_px = 256
+let image_height_px = 256
+
+echo &"P3\n{image_width_px} {image_height_px}\n255\n"
+
+# Stuff some pixels
+var data: seq[uint8] = @[]
+
+for j in countdown(image_height_px - 1, 0):
+    for i in 0 ..< image_width_px:
+        # generate gradient with RGB colors 
+        var r = float(i) / float(image_width_px - 1)
+        var g = float(j) / float(image_height_px - 1)
+        var b = 0.25
+
+        let red   = uint8(255.999 * r)
+        let green = uint8(255.999 * g)
+        let blue  = uint8(255.999 * b)
+
+        var colorRGB = red;
+        colorRGB     = rotateLeftBits(colorRGB, 8) + green;
+        colorRGB     = rotateLeftBits(colorRGB, 8) + blue;
+        
+        # OMG! finally...
+        echo &"{red} {green} {blue}"
+        data.add(colorRGB)
+
+# save it
+stbiw.writeBMP("OMG.png", image_width_px, image_height_px, 3, data)
--- rt-in-one-weekend/src/2.2_creating_an_image_file.cpp
+++ rt-in-one-weekend/src/2.2_creating_an_image_file.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <vector>
-
+#include <string>
 #include <fstream>
+#include <numeric>
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-using namespace std;
+typedef unsigned char channel_value_t;
 
 struct RGB
 {
-    int red;
-    int green;
-    int blue;
+    channel_value_t red;
+    channel_value_t green;
+    channel_value_t blue;
     RGB() = default;
 };
 
-vector<vector<RGB>> get_pixels(int height, int width)
+typedef std::vector<std::vector<RGB>> matrix_t;
+
+matrix_t get_gradient_rgb(int width, int height)
 {
     // define RGB data storage
-    auto data = vector<vector<RGB>>(height, vector<RGB>(width));
+    auto data = matrix_t(height, std::vector<RGB>(width));
 
     for (int j = height - 1; j >= 0; --j)
     {
@@ -28,16 +32,40 @@ vector<vector<RGB>> get_pixels(int height, int width)
             auto g = double(j) / (height - 1);
             auto b = 0.25;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            data[i][j].red = ir;
-            data[i][j].green = ig;
-            data[i][j].blue = ib;
+            data[i][j].red = static_cast<int>(255.999 * r);
+            data[i][j].green = static_cast<int>(255.999 * g);
+            data[i][j].blue = static_cast<int>(255.999 * b);
         }
     }
+
     return data;
+}
+
+bool save_to_ppm(std::string filename, int width, int height, const matrix_t &data)
+{
+    try
+    {
+        std::ofstream output(filename + ".ppm");
+        output << "P3\n"
+               << width << " " << height << "\n255\n";
+        for (int j = height - 1; j >= 0; --j)
+        {
+            for (int i = 0; i < width; ++i)
+            {
+                output
+                    << data[i][j].red << " "
+                    << data[i][j].green << " "
+                    << data[i][j].blue << std::endl;
+            }
+        }
+        output.close();
+    }
+    catch (const std::exception &err)
+    {
+        return false;
+    }
+
+    return true;
 }
 
 int main()
@@ -47,15 +75,27 @@ int main()
     const int image_height = 256;
 
     // generate data
-    auto pixels_data = get_pixels(image_height, image_width);
-    
-    // Render
-    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-    for (auto row : pixels_data)
+    auto pixels_data = get_gradient_rgb(image_width, image_height);
+
+    // save to .ppm file
+    if (!save_to_ppm("image", image_width, image_height, pixels_data))
     {
-        for (auto p : row)
+        std::cout << "Error writing to .ppm file" << std::endl;
+    }
+
+    int data[image_width * image_height * 3];
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < image_width; ++i)
         {
-            cout << p.red << ' ' << p.green << ' ' << p.blue << '\n';
+            data[i][j] = pixels_data[i][j];
         }
     }
+
+    // for (const auto i = 0; )
+    // save to JPEG
+    stbi_write_jpg("image.jpg", image_height, image_width, 3, pixels_data, 100);
+
+    return 0;
 }
