6ee0fe11a3918348238ccbb678b7d9e90d754998
Vic Khov <victorKhovTko@yandex.ru>
2021-08-16 02:49:54 +0300

    made the whole thing work again, v2.0 :monkey_face:

--- rt-in-one-weekend/src/2.2_creating_an_image_file.cpp
+++ rt-in-one-weekend/src/2.2_creating_an_image_file.cpp
@@ -3,74 +3,10 @@
 #include <fstream>
 #include <ctime>
 #include <vector>
-#include <array>
+#include "useful_utils.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-// using these data structures:
-using std::array;
-using std::vector;
-
-array<unsigned char, 3> get_gradient_pixel(const int i, const int j, const int width, const int height)
-{
-    auto r = double(i) / (width - 1);
-    auto g = double(j) / (height - 1);
-    auto b = 0.25;
-
-    array<unsigned char, 3> result = {
-        static_cast<unsigned int>(255.999 * r),
-        static_cast<unsigned int>(255.999 * g),
-        static_cast<unsigned int>(255.999 * b)};
-
-    return result;
-}
-
-// Generate pixel data and write it to array
-vector<unsigned char> get_gradient(const int width, const int height, const int channels_num = 3)
-{
-    vector<unsigned char> pixels(width * height * channels_num);
-    std::cout << "Generating data .................." << std::endl;
-    int index = 0;
-    for (int j = height - 1; j >= 0; --j)
-    {
-        for (int i = 0; i < width; ++i)
-        {
-            auto pixel = get_gradient_pixel(i, j, width, height);
-            pixels[index++] = pixel[0];
-            pixels[index++] = pixel[1];
-            pixels[index++] = pixel[2];
-        }
-    }
-
-    return pixels;
-}
-
-// Get string with current year - month - time
-std::string get_time_str()
-{
-    std::time_t t = std::time(0); // get time now
-    std::tm *now = std::localtime(&t);
-    auto str_timestamp = "_" + std::to_string(now->tm_year + 1900) + '_' + std::to_string(now->tm_mon + 1) + "_" + std::to_string(now->tm_mday) + "_" + std::to_string(now->tm_hour) + "_" + std::to_string(now->tm_min) + "_" + std::to_string(now->tm_sec);
-    return str_timestamp;
-}
-
-void save_to_ppm(const std::string filename, const int width, const int height, const vector<unsigned char> &data, const int channels_num = 3)
-{
-    std::cout << "Trying to write PPM data ........." << std::endl;
-    std::ofstream output(filename + ".ppm");
-    output << "P3\n"
-           << width << " " << height << "\n255\n";
-
-    for (int index = 0; index < width * height * channels_num; index += 3)
-    {
-        output << (unsigned int)data[index] << " "
-               << (unsigned int)data[index + 1] << " "
-               << (unsigned int)data[index + 2] << std::endl;
-    }
-
-    // close file
-    output.close();
-}
+#include "stb_image_write.h" // using to save PPM to JPG
 
 int main()
 {
--- rt-in-one-weekend/src/main.cc
+++ rt-in-one-weekend/src/main.cc
@@ -3,6 +3,8 @@
 #include "vec3.h"
 
 #include "useful_utils.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h" // using to save PPM to JPG
 
 #include <iostream>
 #include <vector>
@@ -19,12 +21,12 @@ color ray_color(const ray &r)
 int main()
 {
     // Image
+    const int channels_num = 3;
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // Camera
-
     auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
     auto focal_length = 1.0;
@@ -34,11 +36,11 @@ int main()
     auto vertical = vec3(0, viewport_height, 0);
     auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
-    // @TODO: do not forget to rewrite this shit
-    vector<vector<RGB>> pixels_data(image_width, vector<RGB>(image_height)); // Defaults to zero initial value[image_width][image_height];
+    // Store data in std::vector
+    int index = 0;
+    vector<unsigned char> pixels_data(image_width * image_height * channels_num);
 
     // Render
-
     std::cout << "P3\n"
               << image_width << " " << image_height << "\n255\n";
 
@@ -51,16 +53,20 @@ int main()
             auto v = double(j) / (image_height - 1);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
             color pixel_color = ray_color(r);
-            //write_color(std::cout, pixel_color);
-            pixels_data[i][j] = write_color_epta(std::cout, pixel_color);
+            write_color(std::cout, pixel_color);
+
+            pixels_data[index++] = static_cast<int>(255.999 * pixel_color.x());
+            pixels_data[index++] = static_cast<int>(255.999 * pixel_color.y());
+            pixels_data[index++] = static_cast<int>(255.999 * pixel_color.z());
         }
     }
 
     // save to .ppm file
-    save_to_ppm((get_time_str() + "_image").c_str(), image_width, image_height, pixels_data);
+    save_to_ppm((get_time_str() + "_blue_grad").c_str(), image_width, image_height, pixels_data);
     // save to .jpg file
-    save_to_jpg(image_width, image_height, pixels_data);
-
+    std::cout << "Trying to write JPG data ........." << std::endl;
+    stbi_write_jpg((get_time_str() + "_blue_grad.jpg").c_str(), image_width, image_height, channels_num, pixels_data.data(), image_width * channels_num);
+    
     std::cerr << "\nDone.\n";
     return 0;
 }
