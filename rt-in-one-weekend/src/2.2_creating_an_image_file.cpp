7026808f3548aa629ff5bd630b7b667daa77bb68
kogrin <44731276+kogrin@users.noreply.github.com>
2021-08-18 22:24:02 +0300

    Ch from 2 to 6 (#1)
    Updated to ch. 6 :brain: :brain: :brain:
    
    Co-authored-by: kogrin <kogron99@gmail.com>
    Co-authored-by: Vic Khov <victorKhovTko@yandex.ru>

--- rt-in-one-weekend/src/2.2_creating_an_image_file.cpp
+++ rt-in-one-weekend/src/2.2_creating_an_image_file.cpp
@@ -2,132 +2,25 @@
 #include <string>
 #include <fstream>
 #include <ctime>
+#include <vector>
+#include "useful_utils.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-
-struct RGB
-{
-    unsigned char red;
-    unsigned char green;
-    unsigned char blue;
-    RGB() = default;
-};
-
-
-// Using template is a trick to allow functions access to
-// 2d array withot usage of pointers
-// or other overcomplicated data structures
-template <size_t rows, size_t cols>
-// Generate pixel data and write it to array
-void get_gradient_rgb(const int width, const int height, RGB (&data)[rows][cols])
-{
-    std::cout << "Generating data .................." << std::endl;
-    int index = 0;
-    for (int j = height - 1; j >= 0; --j)
-    {
-        for (int i = 0; i < width; ++i)
-        {
-            auto r = double(i) / (width - 1);
-            auto g = double(j) / (height - 1);
-            auto b = 0.25;
-
-            data[i][j].red = static_cast<unsigned char>(255.999 * r);
-            data[i][j].green = static_cast<unsigned char>(255.999 * g);
-            data[i][j].blue = static_cast<unsigned char>(255.999 * b);
-        }
-    }
-}
-
-
-template <size_t rows, size_t cols>
-// Write data from array to .ppm file
-bool save_to_ppm(std::string filename, const int width, const int height, const RGB (&data)[rows][cols])
-{
-    try
-    {
-        std::cout << "Trying to write PPM data ........." << std::endl;
-        std::ofstream output(filename + ".ppm");
-        output << "P3\n"
-               << width << " " << height << "\n255\n";
-        for (int j = height - 1; j >= 0; --j)
-        {
-            for (int i = 0; i < width; ++i)
-            {
-                output
-                    << (int)data[i][j].red << " "
-                    << (int)data[i][j].green << " "
-                    << (int)data[i][j].blue << std::endl;
-            }
-        }
-        output.close();
-    }
-    catch (const std::exception &err)
-    {
-        return false;
-    }
-
-    return true;
-}
-
-
-// Get string with current year - month - time
-std::string get_time_str()
-{
-    std::time_t t = std::time(0); // get time now
-    std::tm *now = std::localtime(&t);
-    auto str_timestamp = "_"
-        + std::to_string(now->tm_year + 1900)
-        + '_' + std::to_string(now->tm_mon + 1)
-        + "_" + std::to_string(now->tm_mday)
-        + "_" + std::to_string(now->tm_hour)
-        + "_" + std::to_string(now->tm_min)
-        + "_" + std::to_string(now->tm_sec);
-    return str_timestamp;
-}
-
-
-template <size_t rows, size_t cols>
-void save_to_jpg(const int width, const int height, const RGB (&data)[rows][cols], int channels_num = 3)
-{
-    std::cout << "Trying to write JPG data ........." << std::endl;
-
-    unsigned char tmp[width * height * channels_num];
-
-    int index = 0;
-    for (int j = height - 1; j >= 0; --j)
-    {
-        for (int i = 0; i < width; ++i)
-        {
-            tmp[index++] = data[i][j].red;
-            tmp[index++] = data[i][j].green;
-            tmp[index++] = data[i][j].blue;
-        }
-    }
-
-    stbi_write_jpg((get_time_str() + "_image.jpg").c_str(), width, height, channels_num, tmp, width * channels_num);
-    //stbi_write_jpg("image.jpg", width, height, channels_num, tmp, width * channels_num);
-}
-
+#include "stb_image_write.h" // using to save PPM to JPG
 
 int main()
 {
     // Image parameters
-    const unsigned int channels_num = 3;
-    const unsigned int image_width = 256;
-    const unsigned int image_height = 256;
+    const int channels_num = 3;
+    const int image_width = 256;
+    const int image_height = 256;
 
-    RGB pixels_data[image_width][image_height];
-
-    // generate data
-    get_gradient_rgb(image_width, image_height, pixels_data);
+    // Generate data
+    auto pixels_data = get_gradient(image_width, image_height);
 
     // save to .ppm file
-    if (!save_to_ppm((get_time_str() + "_image").c_str(), image_width, image_height, pixels_data))
-    {
-        std::cout << "Error writing to .ppm file" << std::endl;
-    }
-
-    save_to_jpg(image_width, image_height, pixels_data);
+    save_to_ppm((get_time_str() + "_image").c_str(), image_width, image_height, pixels_data);
+    std::cout << "Trying to write JPG data ........." << std::endl;
+    stbi_write_jpg((get_time_str() + "_image.jpg").c_str(), image_width, image_height, channels_num, pixels_data.data(), image_width * channels_num);
     return 0;
 }
---
+++ rt-in-one-weekend/src/main.cc
@@ -0,0 +1,114 @@
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+#include "useful_utils.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h" // using to save PPM to JPG
+
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+double hit_sphere(const point3 &center, double radius, const ray &r)
+{
+    // os is a vector from origin of the ray to center of the sphere
+    vec3 oc = r.origin() - center;
+    // rewrite sphere eq. in form of quadratic eq.
+    //      (x - Center_x)^2 + (y - Center_y)^2 + (z - Center_z)^2 = r^2
+    //      (P(t) - Center_xyz) * (P(t) - Center_xyz) = r^2;
+    //          -- where `Center_xyz` is vector which points at 
+    //             sphere center coordinates (x, y, z)
+    //
+    //      - note that:    P(t) := orig + t*dir
+    //                      b := dir
+    //      
+    //      (orig + t*b - Center_xyz) * (orig + t*b - Center_xyz) = r^2
+    //      ((orig - Center_xyz) + t*b) * ((orig - Center_xyz) + t*b) - r^2 = 0
+    //      ((orig - Center_xyz)^2 - r^2) + 2*(orig - Center_xyz)*t*b + (t^2)*b^2  = 0
+    //
+    //      - a,b,c - coefficients of quadratic equation: a*t^2 + b^t + c,
+    //          where
+    //              a := b^2
+    //              b := 2*(orig - Center_xyz)*b
+    //              c := (orig - Center_xyz)^2 - r^2
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
+}
+
+color ray_color(const ray &r)
+{
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0)
+    {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+int main()
+{
+    // Image
+    const int channels_num = 3;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+    // Store data in std::vector
+    int index = 0;
+    vector<unsigned char> pixels_data(image_width * image_height * channels_num);
+
+    // Render
+    std::cout << "P3\n"
+              << image_width << " " << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i)
+        {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+
+            pixels_data[index++] = static_cast<int>(255.999 * pixel_color.x());
+            pixels_data[index++] = static_cast<int>(255.999 * pixel_color.y());
+            pixels_data[index++] = static_cast<int>(255.999 * pixel_color.z());
+        }
+    }
+
+    // save to .ppm file
+    save_to_ppm((get_time_str() + "_blue_grad").c_str(), image_width, image_height, pixels_data);
+    // save to .jpg file
+    std::cout << "Trying to write JPG data ........." << std::endl;
+    stbi_write_jpg((get_time_str() + "_blue_grad.jpg").c_str(), image_width, image_height, channels_num, pixels_data.data(), image_width * channels_num);
+
+    std::cerr << "\nDone.\n";
+    return 0;
+}
