e4aa4b5228ff291a7a8582384f9c89c0a0389888
kogrin <kogron99@gmail.com>
2021-08-22 23:28:54 +0300

    Ch 7 Multisample Antialiasing

--- rt-in-one-weekend/src/main.cc
+++ rt-in-one-weekend/src/main.cc
@@ -1,5 +1,6 @@
 #include "rtweekend.h"
 #include "color.h"
+#include "camera.h"
 #include "hittable_list.h"
 #include "sphere.h"
 
@@ -24,12 +25,12 @@ double hit_sphere(const point3 &center, double radius, const ray &r)
     // rewrite sphere eq. in form of quadratic eq.
     //      (x - Center_x)^2 + (y - Center_y)^2 + (z - Center_z)^2 = r^2
     //      (P(t) - Center_xyz) * (P(t) - Center_xyz) = r^2;
-    //          -- where `Center_xyz` is vector which points at 
+    //          -- where `Center_xyz` is vector which points at
     //             sphere center coordinates (x, y, z)
     //
     //      - note that:    P(t) := orig + t*dir
     //                      b := dir
-    //      
+    //
     //      (orig + t*b - Center_xyz) * (orig + t*b - Center_xyz) = r^2
     //      ((orig - Center_xyz) + t*b) * ((orig - Center_xyz) + t*b) - r^2 = 0
     //      ((orig - Center_xyz)^2 - r^2) + 2*(orig - Center_xyz)*t*b + (t^2)*b^2  = 0
@@ -41,50 +42,47 @@ double hit_sphere(const point3 &center, double radius, const ray &r)
     //              c := (orig - Center_xyz)^2 - r^2
     auto a = r.direction().length_squared();
     auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = half_b*half_b - a*c;
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
     if (discriminant < 0)
     {
         return -1.0;
     }
     else
     {
-        return (-half_b - sqrt(discriminant) ) / a;
+        return (-half_b - sqrt(discriminant)) / a;
     }
 }
 
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray &r, const hittable &world)
+{
     hit_record rec;
-    if (world.hit(r, 0, infinity, rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
+    if (world.hit(r, 0, infinity, rec))
+    {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main()
 {
     // Image
-    const int channels_num = 3;
+    const int channels_num = 3; // made by stupid
+
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
+    camera cam;
 
     // World
     hittable_list world;
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
-
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Store data in std::vector
     int index = 0;
@@ -99,15 +97,19 @@ int main()
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i)
         {
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
-
-            pixels_data[index++] = static_cast<int>(255.999 * pixel_color.x());
-            pixels_data[index++] = static_cast<int>(255.999 * pixel_color.y());
-            pixels_data[index++] = static_cast<int>(255.999 * pixel_color.z());
+            color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s)
+            {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
+            auto scaled_px_color = (1.0 / samples_per_pixel) * pixel_color;
+            pixels_data[index++] = static_cast<int>(255.999 * clamp(scaled_px_color.x(), 0.0, 0.999));
+            pixels_data[index++] = static_cast<int>(255.999 * clamp(scaled_px_color.y(), 0.0, 0.999));
+            pixels_data[index++] = static_cast<int>(255.999 * clamp(scaled_px_color.z(), 0.0, 0.999));
         }
     }
 
--- rt-in-one-weekend/src/rtweekend.h
+++ rt-in-one-weekend/src/rtweekend.h
@@ -1,6 +1,7 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
 
+#include <cstdlib>
 #include <cmath>
 #include <limits>
 #include <memory>
@@ -21,6 +22,24 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+// Returns a random real in [0,1).
+
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+// Returns a random real in [min,max).
+
+inline double random_double(double min, double max) {
+    return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common Headers
